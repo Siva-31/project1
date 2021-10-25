8714e3b25c58aa5737233490ef0697f6377aab6b
Siva-31 <sivasoundaravel.com>
2021-10-25 11:08:37 +0530

    commits

---
+++ src/org/filecreate/File.java
@@ -0,0 +1,23 @@
+package org.filecreate;
+
+import java.io.IOException;
+
+public class File {
+
+	public static void main(String[] args)throws IOException {
+		      
+		
+		
+		
+		
+		File txt=new File();
+				
+				//FileUtils.write(Text,"s");
+		
+		
+		
+		
+		
+		
+ }
+}
---
+++ src/org/filecreate/Timing.java
@@ -0,0 +1,19 @@
+package org.filecreate;
+
+import java.util.Date;
+
+public class Timing {
+public static void startTime() {
+	Date d=new Date();
+	System.out.println(d);
+	}
+public static void EndTime() {
+	Date d=new Date();
+	System.out.println(d);
+	}
+public static void main(String[] args) {
+	startTime();
+	EndTime();
+	
+}
+}
